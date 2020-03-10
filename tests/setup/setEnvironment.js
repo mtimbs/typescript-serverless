45112f3140ef8e40c7e3b9ba4f6580c3023827c4
Michael Timbs <mtimbs91@gmail.com>
2020-03-11 00:44:45 +1030

    Add dynamodb env for testing

--- tests/setup/setEnvironment.js
+++ tests/setup/setEnvironment.js
@@ -1,4 +1,5 @@
 const env = {
+  DYNAMODB_TABLE: 'typescript-serverless-local-event-store', // Whatever your table name is goes here
   AWS_REGION: 'local',
   AWS_ACCESS_KEY: 'fake_key',
   AWS_SECRET_KEY: 'fake_secret',
